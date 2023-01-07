c02c63ee087a9aa5fdaa424fbd1718bd479365da
SpencerY426 <122073373+SpencerY426@users.noreply.github.com>
2023-01-06 19:05:35 -0500

    Update AutoSearch.js

--- AutoSearch.js
+++ AutoSearch.js
@@ -1,4 +1,5 @@
 // READ THE READNE.MD FILE IF YOU DON'T UNDERSTAND THIS CODE OR THE NEWTAB PROCESS.
+// A simple but useful piece of code.
 
 for (i=0; i<32; i++) {
     let link = "https://www.bing.com/search?q=" + Math.random()
